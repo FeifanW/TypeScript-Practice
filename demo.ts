37b368f6e6f8d47f67ae9bf40f5ff5b1ed596d96
FeifanW <qihang791@163.com>
2022-02-23 07:29:06 +0800

    练习基础类型和对象类型

--- demo.ts
+++ demo.ts
@@ -14,12 +14,35 @@
 // const count: number = 2022
 // count会继承number上的所有方法
 
-interface Point {
-    x: number,
-    y: number
+// interface Point {
+//     x: number,
+//     y: number
+// }
+
+// const point: Point = {
+//     x:3,
+//     y:2
+// }
+
+// 2.基础类型 null undefined symbol boolean void
+const count: number = 123
+const teacherName: string = 'jack'
+
+// 3.对象类型
+
+class Person {}
+
+const teacher: { name: string ; age: number } = {
+    name: 'jack',
+    age:18
 }
 
-const point: Point = {
-    x:3,
-    y:2
+const num: number[] = [1,2,3]
+
+const jack: Person = new Person()
+
+const getTotal: () => number = () => {
+    return 123
 }
+
+
