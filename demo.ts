1160d2b74037b2eb2613a6c4db507be226c87c0d
FeifanW <qihang791@163.com>
2022-02-23 07:13:18 +0800

    静态类型深度

--- demo.ts
+++ demo.ts
@@ -4,9 +4,22 @@
 // b = 456
 
 // type Point = {x:number, y:number}
-interface Point {x:number, y:number}
-function tsDemo(data: Point){
-    // console.log("test") 
-    return Math.sqrt(data.x ** 2 + data.y ** 2)
+// interface Point {x:number, y:number}
+// function tsDemo(data: Point){
+//     // console.log("test") 
+//     return Math.sqrt(data.x ** 2 + data.y ** 2)
+// }
+// tsDemo({x:1, y:2})
+
+// const count: number = 2022
+// count会继承number上的所有方法
+
+interface Point {
+    x: number,
+    y: number
 }
-tsDemo({x:1, y:2})
+
+const point: Point = {
+    x:3,
+    y:2
+}
