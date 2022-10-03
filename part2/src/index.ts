3da0449a3830c4e0669acd420d082fded43c63c7
FeifanW <qihang791@163.com>
2022-10-03 17:09:24 +0800

    学习使用wepack打包ts代码

--- part2/src/index.ts
+++ part2/src/index.ts
@@ -2,4 +2,12 @@ function sum(a:number, b:number):number {
     return a + b
 }
 
+const aa = {
+    name:"123"
+}
+aa.name = "name"
+console.log(aa)
+
+console.log(Promise)
+
 console.log(sum(123,456))
--- part2/webpack.config.js
+++ part2/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 // webpack中所有的配置信息都应该写在module.exports中
 module.exports = {
@@ -9,7 +11,10 @@ module.exports = {
         // 指定打包文件的目录
         path: path.resolve(__dirname,'dist'),
         // 打包后文件的文件
-        filename:"bundle.js"
+        filename:"bundle.js",
+        environment:{
+            arrowFunction: false
+        }
     },
     // 指定webpack打包时要使用的模块
     module: {
@@ -19,10 +24,48 @@ module.exports = {
                 // test指定的是规则生效的文件
                 test:/\.ts$/,
                 // 要使用的loader
-                use:'ts-loader',
+                use:[
+                    // 配置babel
+                    {
+                        // 指定加载器
+                        loader:"babel-loader",
+                        // 设置babel
+                        options:{
+                            // 设置预定义的环境
+                            presets:[
+                                [
+                                    // 指定环境的插件
+                                    "@babel/preset-env",
+                                    // 配置信息
+                                    {
+                                        // 要兼容的目标浏览器
+                                        targets:{
+                                            "chrome":"88",
+                                            "ie":"11"
+                                        },
+                                        // 指定corejs的版本
+                                        "corejs":"3",
+                                        // 使用corejs的方式"usage"表示按需加载
+                                        "useBuiltIns":"usage"
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    'ts-loader'
+                ],
                 // 要排除的文件
                 exclude:/node-modules/
-            }
-        ]
+            },
+        ],
+    },
+    // 配置webpack插件
+    plugins:[
+        new HTMLWebpackPlugin(),
+        new CleanWebpackPlugin(),
+    ],
+    // 用来设置模块
+    resolve:{
+        extensions: ['.js','.ts']
     }
 }
