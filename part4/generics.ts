09221d2e6ea02ac1e3f50ac903f26dd1f5f6d2e6
FeifanW <qihang791@163.com>
2022-10-22 18:29:54 +0800

    约束泛型

--- part4/generics.ts
+++ part4/generics.ts
@@ -9,3 +9,25 @@ function swap<T, U>(tuple:[T,U]):[U,T]{
     return [tuple[1], tuple[0]]
 }
 const result2  = swap(['string', 123])
+
+function echoWithArr<T>(arg:T[]): T[] {
+    console.log(arg.length)
+    return arg
+}
+
+const arrs = echoWithArr([1, 2, 3])
+
+// 可以用接口interface来约束泛型
+interface IWithLength {
+    length: number
+}
+
+function echoWithLength<T extends IWithLength>(arg: T):T {
+    console.log(arg.length)
+    return arg
+}
+
+const str = echoWithLength('str')
+const obj = echoWithLength({length:10, width:10})
+const arr2 = echoWithLength([1,2,3])
+// echoWithLength(13)     // 约束泛型必须有length属性
