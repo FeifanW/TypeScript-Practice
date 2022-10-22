44fa0afc1780c655bd0311298dbb7ff6b5e6af1a
FeifanW <qihang791@163.com>
2022-10-22 19:10:24 +0800

    练习泛型

--- part4/generics.ts
+++ part4/generics.ts
@@ -31,3 +31,38 @@ const str = echoWithLength('str')
 const obj = echoWithLength({length:10, width:10})
 const arr2 = echoWithLength([1,2,3])
 // echoWithLength(13)     // 约束泛型必须有length属性
+
+class Queue<T> {   // 类可以用泛型来约束
+    private data = []
+    push(item: T) {
+        return this.data.push(item)
+    }
+    pop():T {
+        return this.data.shift()
+    }
+}
+
+const queue = new Queue<number>()
+queue.push(1)
+console.log(queue.pop().toFixed())
+
+interface KeyPair<T, U> {
+    key: T
+    value: U
+}
+let kp1: KeyPair<number, string> = {key:1, value:'string'}
+let kp2: KeyPair<string, number> = {key:'str', value:2}
+let arr:number[] = [1,2,3]
+let arrTwo:Array<number> = [1,2,3]
+
+
+
+
+
+
+
+
+
+
+
+
